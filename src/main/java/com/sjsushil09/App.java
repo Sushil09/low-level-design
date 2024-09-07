3c1e068d993f9284b4d0f2cf72ecbe50c4cd0ab7
Sushil <sjsushil09@gmail.com>
2024-09-07 13:55:49 +0530

    Initialzing the app

---
+++ src/main/java/com/sjsushil09/App.java
@@ -0,0 +1,13 @@
+package com.sjsushil09;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

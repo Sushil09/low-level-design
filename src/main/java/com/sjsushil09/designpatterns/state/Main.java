a4d4dd843e02f111ca561fbb7128c671adc64db0
Sushil <sjsushil09@gmail.com>
2024-09-23 02:32:15 +0530

    State Design Pattern

--- src/main/java/com/sjsushil09/designpatterns/state/Main.java
+++ src/main/java/com/sjsushil09/designpatterns/state/Main.java
@@ -1,5 +1,86 @@
 package com.sjsushil09.designpatterns.state;
 
-public class Main {
+interface FanState{
+    public void turnDown(FanContext fanContext);
+    public void turnUp(FanContext fanContext);
+}
 
+class IdleState implements FanState {
+
+    @Override
+    public void turnDown(FanContext fanContext) {
+        System.out.println("Fan is already off");
+    }
+
+    @Override
+    public void turnUp(FanContext fanContext) {
+        System.out.println("Turning on at low speed");
+        fanContext.setFanState(new DecreaseSpeed());
+    }
+}
+
+class IncreaseSpeed implements FanState {
+
+    @Override
+    public void turnDown(FanContext fanContext) {
+        System.out.println("Lowering speed of fan");
+        fanContext.setFanState(new DecreaseSpeed());
+    }
+
+    @Override
+    public void turnUp(FanContext fanContext) {
+        System.out.println("Increasing the speed");
+        fanContext.setFanState(new IncreaseSpeed());
+    }
+}
+
+class DecreaseSpeed implements FanState {
+
+    @Override
+    public void turnDown(FanContext fanContext) {
+        System.out.println("Switching off the fan");
+        fanContext.setFanState(new IdleState());
+    }
+
+    @Override
+    public void turnUp(FanContext fanContext) {
+        System.out.println("Increase speed of fan");
+        fanContext.setFanState(new IncreaseSpeed());
+    }
+}
+
+
+
+class FanContext {
+    private FanState fanState;
+
+    FanContext() {
+        fanState = new IdleState();
+    }
+
+    public void setFanState(FanState state) {
+        this.fanState = state;
+    }
+
+    public void turnUp() {
+        fanState.turnUp(this);
+    }
+
+    public void turnDown() {
+        fanState.turnDown(this);
+    }
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+        FanContext fan = new FanContext(); //Idle state
+
+        fan.turnUp();
+        fan.turnDown();
+        fan.turnDown();
+
+
+
+    }
 }
